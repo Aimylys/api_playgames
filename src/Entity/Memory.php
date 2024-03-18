f0bbe4a99b3341fba63b84d9bf45cd9bc961107f
0uzap <provostristan@gmail.com>
2024-03-18 21:54:59 +0100

    début api moi

--- src/Entity/Memory.php
+++ src/Entity/Memory.php
@@ -5,17 +5,23 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\MemoryRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 
 #[ORM\Entity(repositoryClass: MemoryRepository::class)]
-#[ApiResource]
+#[ApiResource(paginationItemsPerPage: 20,
+operations:[new Get(normalizationContext: ['groups' => 'memory:item']),
+            new GetCollection(normalizationContext: ['groups' => 'memory:list'])])]
 class Memory
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['memory:list', 'memory:item', 'memoryscore:item'])]
     private ?int $id = null;
 
     #[ORM\Column(nullable: true)]
+    #[Groups(['memory:list', 'memory:item', 'memoryscore:item'])]
     private ?int $scoreM = null;
 
     #[ORM\ManyToOne(inversedBy: 'scoreMemory')]
