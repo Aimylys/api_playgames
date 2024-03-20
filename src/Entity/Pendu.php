abb4734dd0247eaabfa5d3cb74df79b6fab18af9
Aimylys <denaison.emilie@gmail.com>
2024-03-20 14:07:04 +0100

    maj api

--- src/Entity/Pendu.php
+++ src/Entity/Pendu.php
@@ -6,27 +6,36 @@ use App\Repository\PenduRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;//ajoute
+use ApiPlatform\Metadata\Put;//modifie
+use ApiPlatform\Metadata\Patch;//modifie partiellement
 
 #[ORM\Entity(repositoryClass: PenduRepository::class)]
 #[ApiResource(paginationItemsPerPage: 20,
 operations:[new Get(normalizationContext: ['groups' => 'pendu:item']),
-            new GetCollection(normalizationContext: ['groups' => 'pendu:list'])])]
+            new GetCollection(normalizationContext: ['groups' => 'pendu:list']),
+            new Post(normalizationContext: ['groups' => 'pendu:list']),
+            new Put(normalizationContext: ['groups' => 'pendu:item']),
+            new Patch(security: "is_granted('ROLE_ADMIN') or object == user"),
+            ])]
 class Pendu
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['pendu:list', 'pendu:item', 'penduscore:item'])]
+    #[Groups(['pendu:list', 'pendu:item', 'user:item'])]
     private ?int $id = null;
 
     #[ORM\Column(nullable: true)]
-    #[Groups(['pendu:list', 'pendu:item', 'penduscore:item'])]
+    #[Groups(['pendu:list', 'pendu:item', 'user:item'])]
     private ?int $score = null;
 
     #[ORM\ManyToOne(inversedBy: 'pendus')]
+    #[Groups(['pendu:list', 'pendu:item', 'user:item'])]
     private ?User $user = null;
 
     public function getId(): ?int
